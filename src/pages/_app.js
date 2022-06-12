dd230c6c19b2516f19fda227a4be268400e6424b
Nadine <nadlgit@users.noreply.github.com>
2022-06-12 16:53:53 -0400

    feat: modified <head> tags repartition between _document and _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import 'styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
           rel="stylesheet"
