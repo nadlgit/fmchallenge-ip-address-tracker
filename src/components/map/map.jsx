cb1610cddcfffaabd0d1423d4b15c7f07abc0f46
Nadine <nadlgit@users.noreply.github.com>
2022-06-14 11:13:13 -0400

    feat: style updates related to the map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,8 +1,7 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
-// import iconLocation from './icon-location.svg';
 
 export const Map = ({ latitude = 0, longitude = 0 }) => {
   const position = [latitude, longitude];
@@ -11,15 +10,16 @@ export const Map = ({ latitude = 0, longitude = 0 }) => {
       center={position}
       zoom={13}
       zoomControl={false}
-      // scrollWheelZoom={'center'}
-      // touchZoom={'center'}
+      scrollWheelZoom={'center'}
+      touchZoom={'center'}
+      dragging={false}
       style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position} />
+      <Marker position={position} interactive={false} />
     </MapContainer>
   );
 };
