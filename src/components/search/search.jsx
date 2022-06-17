1e2d2ad3f5de6af225a465d2b84edfc7e5d61037
Nadine <nadlgit@users.noreply.github.com>
2022-06-17 00:08:16 -0400

    feat: updated validation for user input

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,17 +1,22 @@
 import styles from './search.module.css';
 import iconArrow from './icon-arrow.svg';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { isValidIP } from 'utils/helpers';
 
 export const Search = ({ onSearch = () => {} }) => {
   const input = useRef();
+  const [error, setError] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const value = input.current.value?.trim();
-    if (isValidIP(value)) {
+    if (value === '' || isValidIP(value)) {
+      setError('');
       input.current.value = '';
-      onSearch(value);
+      value && onSearch(value);
+    } else {
+      setError('Please enter a valid IP v4 address');
     }
   };
 
