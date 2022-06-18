7b6854803ea77c762f600086034dccb514fc3413
Nadine <nadlgit@users.noreply.github.com>
2022-06-18 11:41:22 -0400

    feat: update map center on location change

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,24 +7,25 @@ const markerIcon = new L.Icon({
   iconUrl: iconLocation.src,
 });
 
-const MapPositionHandler = ({ position }) => {
-  useMap().setView(position);
+const MapPositionHandler = ({ position, zoom }) => {
+  useMap().setView(position, zoom);
   return null;
 };
 
 export const Map = ({ latitude = 0, longitude = 0 }) => {
   const position = [latitude, longitude];
+  const defaultZoom = 13;
   return (
     <MapContainer
       center={position}
-      zoom={13}
+      zoom={defaultZoom}
       zoomControl={false}
       scrollWheelZoom={'center'}
       touchZoom={'center'}
       dragging={false}
       style={{ height: '100%', width: '100%' }}
     >
-      <MapPositionHandler position={position} />
+      <MapPositionHandler position={position} zoom={defaultZoom} />
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
