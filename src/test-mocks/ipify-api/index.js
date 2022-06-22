173a5988a5230ed1d624b488373d44afc302de5c
Nadine <nadlgit@users.noreply.github.com>
2022-06-22 03:43:49 -0400

    test: simplification + useIpLocation() hook

--- src/test-mocks/ipify-api/index.js
+++ src/test-mocks/ipify-api/index.js
@@ -1,16 +1,16 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-const API_URL = 'https://api.ipify.org/';
+const API_URL = 'https://api.ipify.org';
 
 export const OWN_IP_MOCK_VALUE = '9.88.77.6';
 
-export const okHandler = rest.get(API_URL, (req, res, ctx) => {
-  return res(ctx.status(200), ctx.text(OWN_IP_MOCK_VALUE));
-});
+export const server = setupServer(
+  rest.get(API_URL, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.text(OWN_IP_MOCK_VALUE));
+  })
+);
 
 export const errorHandler = rest.get(API_URL, (req, res, ctx) => {
   return res(ctx.status(500));
 });
-
-export const server = setupServer(okHandler);
--- src/utils/use-ip-location/use-ip-location.test.js
+++ src/utils/use-ip-location/use-ip-location.test.js
@@ -1,3 +1,67 @@
+import { useIpLocation } from './use-ip-location';
+import 'whatwg-fetch';
+import { server, testData, DEFAULT_DATA } from 'test-mocks/internal-iplocation-api';
+import { renderHook, waitFor } from '@testing-library/react';
+import { handleOwnIp } from 'utils/helpers';
+
+jest.mock('utils/helpers');
+handleOwnIp.mockImplementation(async (ip) => Promise.resolve(ip));
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
 describe('useIpLocation() hook', () => {
-  it.todo('todo');
+  it.each(['1.2.3.4', ''])('should call handleOwnIp() with received IP "%s"', async (ip) => {
+    const { result } = renderHook(() => useIpLocation(ip));
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual({});
+
+    await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+    expect(handleOwnIp).toHaveBeenLastCalledWith(ip);
+  });
+
+  it('should return default data when no IP provided', async () => {
+    const { result } = renderHook(() => useIpLocation());
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual({});
+
+    await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual(DEFAULT_DATA);
+  });
+
+  it.each(testData)('should return data for IP "$ip"', async (val) => {
+    const { result } = renderHook(() => useIpLocation(val.ip));
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual({});
+
+    await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual(val);
+  });
+
+  it('should return error when API error', async () => {
+    const testIp = '1.2.3.4';
+    expect(testData.map((item) => item.ip)).not.toContain(testIp);
+    const { result } = renderHook(() => useIpLocation(testIp));
+    expect(result.current.isLoading).toBeTruthy();
+    expect(result.current.error).toBeNull();
+    expect(result.current.data).toEqual({});
+
+    await waitFor(() => expect(result.current.isLoading).toBeFalsy());
+    expect(result.current.error).not.toBeNull();
+    expect(result.current.data).toEqual({});
+  });
 });
